5edb5b5f396b8a59abb27b4fcce7ccffdc758bce
Dman2705 <dhiren.chander@gmail.com>
2024-10-01 02:59:40 -0400

    Added Posts, Comments, Users, Authentication

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -14,3 +14,20 @@ CREATE TABLE posts (
 );
 CREATE INDEX posts_created_at_index ON posts (created_at);
 CREATE TRIGGER update_posts_updated_at BEFORE UPDATE ON posts FOR EACH ROW EXECUTE FUNCTION set_updated_at_to_now();
+CREATE TABLE comments (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    body TEXT NOT NULL,
+    author TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    post_id UUID NOT NULL
+);
+CREATE INDEX comments_created_at_index ON comments (created_at);
+CREATE INDEX comments_post_id_index ON comments (post_id);
+CREATE TABLE users (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    email TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
+    locked_at TIMESTAMP WITH TIME ZONE DEFAULT NULL,
+    failed_login_attempts INT DEFAULT 0 NOT NULL
+);
+ALTER TABLE comments ADD CONSTRAINT comments_ref_post_id FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE;
